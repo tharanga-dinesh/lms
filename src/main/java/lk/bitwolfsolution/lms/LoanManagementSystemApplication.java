19a7ef4336be5c114165b8ef2b7c0b2f957176b5
tharaͷga-diͷ3sh <30534799+tharanga-dinesh@users.noreply.github.com>
2020-10-20 22:36:06 +0530

    initial commit

---
+++ src/main/java/lk/bitwolfsolution/lms/LoanManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.bitwolfsolution.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanManagementSystemApplication.class, args);
+	}
+
+}
